8fb236b33a47fc564e8e8af53293b6f30268e7ac
andreia isabeli <101262563+andreiaisabeli@users.noreply.github.com>
2022-10-06 08:41:27 -0300

    Add files via upload

---
+++ reserva_teatro/js/teatro.js
@@ -0,0 +1,9 @@
+const frm = document.querySelector("form");
+const dvPalco = document.querySelector("#divPalco");
+
+//numero de poltronas
+const POLTRONAS = 240;
+
+//vetor c as poltronas reservadas 
+const reservadas = [];
+
